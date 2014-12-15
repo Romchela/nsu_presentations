b9d94fe791b4bed74951035f5a8d942f825afa5b
Romchela <romchela@mail.ru>
2014-12-15 22:28:01 +0600

    add index

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Short description for index.php
+ *
+ * @package index
+ * @author romchela <romchela@romchelaPC>
+ * @version 0.1
+ * @copyright (C) 2014 romchela <romchela@romchelaPC>
+ * @license MIT
+ */
+
+
+?>
+
+
+HELLO!
